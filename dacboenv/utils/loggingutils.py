b5447f96aea9d6ecda2c8026b17a8cba67e869b6
Thibaut Klenke <thibaut.klenke@stud.uni-hannover.de>
2025-09-06 12:12:31 +0200

    Update logger

--- dacboenv/utils/loggingutils.py
+++ dacboenv/utils/loggingutils.py
@@ -10,6 +10,8 @@ from pathlib import Path
 from typing import Any
 
 import numpy as np
+from hydra.core.hydra_config import HydraConfig
+from hydra.types import RunMode
 from rich.logging import RichHandler
 
 
@@ -87,3 +89,35 @@ def log_python_env(log_file: str | Path = "env_log.txt") -> None:
         f.write(f"Python Version: {sys.version}\n")
         f.write(f"Python Executable: {sys.executable}\n")
     log_pip_freeze(log_file)
+
+
+def dump_logs(log_data: dict, filename: str) -> None:
+    """Dump log dict in jsonl format.
+
+    This appends one json dict line to the filename.
+
+    Parameters
+    ----------
+    log_data : dict
+        Data to log, must be json serializable.
+    filename : str
+        Filename without path. The path will be either the
+        current working directory or if it is called during
+        a hydra session, the hydra run dir will be the log
+        dir.
+    """
+    log_data_str = json.dumps(log_data) + "\n"
+
+    try:
+        # Check if we are in a hydra context
+        hydra_cfg = HydraConfig.instance().get()
+        if hydra_cfg.mode == RunMode.RUN:
+            directory = Path(hydra_cfg.run.dir)
+        else:  # MULTIRUN
+            directory = Path(hydra_cfg.sweep.dir) / hydra_cfg.sweep.subdir
+
+    except Exception:  # noqa: BLE001
+        directory = Path()
+    filepath = directory / filename
+    with open(filepath, mode="a") as file:
+        file.writelines([log_data_str])

be64f83a19bf372ec5f57f9f3a2769453dc179db
Thibaut Klenke <thibaut.klenke@stud.uni-hannover.de>
2025-10-07 12:10:21 +0200

    PPO Training scripts

---
+++ scripts/ppo_run_singleobs_cluster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -t 18:00:00
+#SBATCH -J "dacboenv"
+#SBATCH --cpus-per-task=128
+#SBATCH -p normal
+
+cd /scratch/hpc-prf-intexml/tklenke/repos/dacboenv/scripts
+source /scratch/hpc-prf-intexml/tklenke/repos/dacboenv/.venv/bin/activate
+python concept_single.py

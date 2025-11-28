aa1eb026ce068987f8124eb10344adeef817dc66
Thibaut Klenke <thibaut.klenke@stud.uni-hannover.de>
2025-11-28 15:02:19 +0100

    PPO scripts

---
+++ scripts/old/ppo_step_cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -t 24:00:00
+#SBATCH -J "dacboenv"
+#SBATCH --cpus-per-task=128
+#SBATCH --mem=96G
+#SBATCH -p normal
+
+cd /scratch/hpc-prf-intexml/tklenke/repos/dacboenv/scripts
+source /scratch/hpc-prf-intexml/tklenke/repos/dacboenv/.venv/bin/activate
+python ppo_step.py --reward "$1" --seed "$2"

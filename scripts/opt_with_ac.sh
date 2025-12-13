c92aa600482551fa6c635bab159e9023a4284bf0
benjamc <c.benjamins@ai.uni-hannover.de>
2025-12-13 16:10:01 +0100

    add script

---
+++ scripts/opt_with_ac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -t 24:00:00
+#SBATCH -J "ac4dacbo"
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH -p normal
+#SBATCH --array=1-11
+
+
+BASE="carps.run 'hydra.searchpath=[pkg://dacboenv/configs]'"
+SEED="seed=range(1,11)"
+CLUSTER="+cluster=cpu_noctua"
+
+python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac +env=default +instances=bbob2d_5seeds optimizer.smac_cfg.scenario.n_workers=1

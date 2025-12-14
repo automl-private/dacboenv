18f37bb3506d93e95eadb4387869e3f87764ef8b
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2025-12-14 09:55:30 +0100

    new configs

--- scripts/opt_with_ac.sh
+++ scripts/opt_with_ac.sh
@@ -4,11 +4,12 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH -p normal
-#SBATCH --array=1-11
+#SBATCH --array=1-10
 
 
-BASE="carps.run 'hydra.searchpath=[pkg://dacboenv/configs]'"
+BASE="carps.run hydra.searchpath=[pkg://dacboenv/configs]"
 SEED="seed=range(1,11)"
 CLUSTER="+cluster=cpu_noctua"
 
-python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac +env=default +instances=bbob2d_5seeds optimizer.smac_cfg.scenario.n_workers=1
+# python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac +env=default +instances=bbob2d_5seeds optimizer.smac_cfg.scenario.n_workers=1
+python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac_inccost +env=default +instances=bbob2d_3seeds optimizer.smac_cfg.scenario.n_workers=1

fea673c6f5cf03d97f1cc10c3cb7619991a5adf0
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2025-12-15 14:13:33 +0100

    update

--- scripts/opt_with_ac.sh
+++ scripts/opt_with_ac.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -t 24:00:00
 #SBATCH -J "ac4dacbo"
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=11
 #SBATCH --mem=16G
 #SBATCH -p normal
-#SBATCH --array=1-10
+#SBATCH --array=1-5
 
 
 BASE="carps.run hydra.searchpath=[pkg://dacboenv/configs]"
@@ -12,4 +12,6 @@ SEED="seed=range(1,11)"
 CLUSTER="+cluster=cpu_noctua"
 
 # python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac +env=default +instances=bbob2d_5seeds optimizer.smac_cfg.scenario.n_workers=1
-python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac_inccost +env=default +instances=bbob2d_3seeds optimizer.smac_cfg.scenario.n_workers=1
+# python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac_inccost +env=default +instances=bbob2d_3seeds optimizer.smac_cfg.scenario.n_workers=1
+# python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac +task=dacboenv_epdonescaledpluslogregret +instances=bbob2d_1_3seeds optimizer.smac_cfg.scenario.n_workers=10
+python -m $BASE seed=$SLURM_ARRAY_TASK_ID +opt=smac +task=dacboenv_epdonescaledpluslogregret_trialsleft +instances=bbob2d_1_3seeds optimizer.smac_cfg.scenario.n_workers=10
--- scripts/opt_with_cmaes.sh
+++ scripts/opt_with_cmaes.sh
@@ -4,6 +4,12 @@
 #SBATCH --cpus-per-task=11
 #SBATCH --mem=16G
 #SBATCH -p normal
-#SBATCH --array=1-5
+#SBATCH --array=1-1
 
-python -m dacboenv.optimize_via_cma +opt=cma  +task=dacboenv_epdonescaled +instances=bbob2d_3seeds seed=$SLURM_ARRAY_TASK_ID +env/instance_selector=random
+export HYDRA_FULL_ERROR=1
+# python -m dacboenv.optimize_via_cma +opt=cma  +task=dacboenv_epdonescaled +instances=bbob2d_3seeds seed=$SLURM_ARRAY_TASK_ID +env/instance_selector=random
+# python -m dacboenv.optimize_via_cma +opt=cma  +task=dacboenv_epdonescaled +instances=bbob2d_1_3seeds seed=$SLURM_ARRAY_TASK_ID +env/instance_selector=random
+# python -m dacboenv.optimize_via_cma +opt=cma  +task=dacboenv_epdonescaledpluslogregret +instances=bbob2d_1_3seeds seed=$SLURM_ARRAY_TASK_ID +env/instance_selector=random
+python -m dacboenv.optimize_via_cma +opt=cma  +task=dacboenv_epdonescaledpluslogregret_trialsleft +instances=bbob2d_1_3seeds seed=$SLURM_ARRAY_TASK_ID +env/instance_selector=random
+# python -m dacboenv.optimize_via_cma +opt=cma  +task=dacboenv_epdonescaledpluslogregret_trialsleft +instances=bbob2d_1_3seeds seed=1 +env/instance_selector=random n_workers=1
+# python -m dacboenv.optimize_via_cma +opt=cma  +task=dacboenv_epdonescaled +instances=bbob2d_3seeds seed=1 +env/instance_selector=random n_workers=4 n_trials=100

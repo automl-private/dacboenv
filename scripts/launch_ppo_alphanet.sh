cec06a4aa827934883be834560dc55decabf630a
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2025-12-31 18:34:33 +0100

    launch ppo versions

--- scripts/launch_ppo_alphanet.sh
+++ scripts/launch_ppo_alphanet.sh
@@ -3,7 +3,7 @@ tasks=(
     # "+task=dacboenv_epdonescaledpluslogregret_budgetpercentage_wei"
     # "+task=dacboenv_epdonescaledpluslogregret_wei"
     # "+task=dacboenv_epdonescaledpluslogregret"
-    # "+task=dacboenv_sawei"
+    "+task=dacboenv_sawei"
     "+task=dacboenv_sawei2"
 )
 instance_sets=(
@@ -12,12 +12,20 @@ instance_sets=(
     "+instances=bbob2d_8_3seeds"
     "+instances=bbob2d_3seeds"
 )
+opts=(
+    "+opt=ppo_alphanet"
+    "+opt=ppo_alphanet2"
+    "+opt=ppo_alphanet3"
+)
 
 for task in "${tasks[@]}"
 do
     for instance_set in "${instance_sets[@]}"
     do
-        echo Launch for: $task $instance_set
-        sbatch scripts/opt_ppo_norm_alphanet.sh $instance_set $task
+        for opt in "${opts[@]}"
+        do
+            echo Launch for: $task $instance_set $opt
+            sbatch scripts/opt_ppo_norm_alphanet.sh $instance_set $task $opt
+        done
     done
 done

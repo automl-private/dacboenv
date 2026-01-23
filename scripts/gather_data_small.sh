0a0b270cb5353c3f0ea564fb3dc1846dd8986004
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2026-01-23 18:28:18 +0100

    update

--- scripts/gather_data_small.sh
+++ scripts/gather_data_small.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=gathers
 #SBATCH --partition=normal
-#SBATCH --time=1:00:00            
-#SBATCH --cpus-per-task=6
+#SBATCH --time=3:00:00            
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=24GB    
 #SBATCH --output=gather_data/slurm-%j.out     # stdout log
 #SBATCH --error=gather_data/slurm-%j.err      # stderr log
@@ -13,6 +13,10 @@ source .env/bin/activate
 
 # sbatch scripts/generate_report.sh
 
+# python -m carps.analysis.gather_data \
+# 	'--rundir=["runs_eval/PPO-AlphaNet*","runs_eval/NoOpPolicy","runs_eval/SAWEI","runs_eval/SMAC-AC--dacbo_Cepisode_length_scaled_plus_logregret_AWEI-cont_Ssawei_Repisode_finished_scaled*","runs_eval/SMAC-AC--dacbo_Csymlogregret_AWEI-cont_Ssawei_Rsymlogregret*"]' \
+# 	--outdir=results_alphanet2
+
 python -m carps.analysis.gather_data \
-	'--rundir=["runs_eval/PPO-AlphaNet*","runs_eval/NoOpPolicy","runs_eval/SAWEI","runs_eval/SMAC-AC--dacbo_Cepisode_length_scaled_plus_logregret_AWEI-cont_Ssawei_Repisode_finished_scaled*","runs_eval/SMAC-AC--dacbo_Csymlogregret_AWEI-cont_Ssawei_Rsymlogregret*"]' \
-	--outdir=results_alphanet2
+	'--rundir=["runs_eval_icml","runs_eval/DefaultPolicy","runs_eval/Random","runs_eval/SAWEI-P"]' \
+	--outdir=results_icml

d53f43eb5d1023e2fcf97d16cb753dfa9ad23826
Carolin Benjamins <c.benjamins@ai.uni-hannover.de>
2026-01-26 16:39:00 +0100

    fix

--- scripts/launch_state_inspection.sh
+++ scripts/launch_state_inspection.sh
@@ -9,20 +9,24 @@ CLUSTER="+cluster=cpu_noctua"
 UCB_ACT="+env/action=ucb_beta_continuous"
 WEI_ACT="+env/action=wei_alpha_continuous"
 
-POLICY_DEFAULT="+policy=default"
+POLICY_DEFAULT="+policy=defaultaction"
 POLICY_JUMP05="+policy=jump_05"
 POLICY_RANDOM="+policy=random"
 
-ENV="+env=base +env/opt=base +env/reward=ep_done_scaled +env/obs=all"
-BASERUNDIR='baserundir=runs_statespace/${action_space_id}'
+ENV="+env=base +env/opt=base +env/reward=ep_done_scaled +env/obs=all +env/refperf=defaultaction"
+BASERUNDIR='baserundir=runs_statespace_icml/${action_space_id}'
+
+# CLUSTER=""
+# SEED="seed=1"
+# TASKALL=$TASK8
 
 # 1
-python -m $BASE $SEED $TASKALL +eval=base $ENV $WEI_ACT $POLICY_DEFAULT $CLUSTER $BASERUNDIR --multirun &
+python -m $BASE $SEED $TASKALL $ENV +eval=base $WEI_ACT $POLICY_DEFAULT $CLUSTER $BASERUNDIR --multirun &
 
 # 2, 3
 python -m $BASE $SEED $TASKALL +eval=base $ENV $WEI_ACT $POLICY_JUMP05 $CLUSTER $BASERUNDIR --multirun &
 
-# 4
+# # 4
 python -m $BASE $SEED $TASK20 +eval=base $ENV $WEI_ACT $POLICY_RANDOM $CLUSTER $BASERUNDIR --multirun &
 python -m $BASE $SEED $TASK20 +eval=base $ENV $UCB_ACT $POLICY_RANDOM $CLUSTER $BASERUNDIR --multirun &
 
